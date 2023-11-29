1041bb782c031023ce0c147666a9b5af569265da
Mattpott <mattpotter562@gmail.com>
2023-11-29 13:59:49 -0500

    Base

---
+++ cs412_vertex_cover.py
@@ -0,0 +1,25 @@
+"""
+    name:  Matthew Potter
+"""
+
+# Exact Algorithm:
+# https://www.mdpi.com/2227-7390/7/7/603
+
+
+# Approximation Algorithm:
+# https://tandy.cs.illinois.edu/dartmouth-cs-approx.pdf
+# https://www.ijser.org/researchpaper/Optimal-Algorithm-for-Solving-Vertex-Cover-Problem-in-Polynomial-Time.pdf
+# https://i11www.iti.kit.edu/_media/teaching/sommer2010/approximationsonlinealgorithmen/lp.pdf
+# https://cpsc.yale.edu/sites/default/files/files/tr404.pdf
+# https://www.personal.kent.edu/~rmuhamma/Algorithms/MyAlgorithms/AproxAlgor/vertexCover.htm
+
+
+# All modules for CS 412 must include a main method that allows it
+# to be imported and invoked from other python scripts
+def main():
+    # your code here
+    pass
+
+
+if __name__ == "__main__":
+    main()
